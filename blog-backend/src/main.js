942a2c517f77edd81ad700802b74df684ed3b9fc
MJ98 <limminjeong98@naver.com>
2021-03-25 16:21:23 +0900

    first commit

---
+++ blog-backend/src/main.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const mongoose = require('mongoose');
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
+
+import api from './api';
+// import createFakeData from './createFakeData';
+// createFakeData는 최초한번만 실행하고 주석처리하기
+import jwtMiddleware from './lib/jwtMiddleware';
+
+const { PORT, MONGO_URI } = process.env;
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useFindAndModify: false })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // createFakeData();
+    // createFakeData는 최초한번만 실행하고 주석처리하기
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+const app = new Koa();
+const router = new Router();
+
+router.use('/api', api.routes());
+
+app.use(bodyParser());
+app.use(jwtMiddleware);
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(__dirname, '../../blog-frontend/build');
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
+
+const port = PORT || 4000;
+
+app.listen(port, () => {
+  console.log('Listening to port %d', port);
+});
---
+++ blog-frontend/README.md
@@ -0,0 +1,8 @@
+### In terminal move to the blog-backend directory,
+
+type `npm install`
+
+### In terminal move to the blog-frontend directory,
+
+type `npm install`
+Run frontend-server, type `yarn start`
